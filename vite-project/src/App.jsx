0f6787453547fadc4f9637ed7bddc18f435d1fa5
kishore <kk7315102@gmail.com>
2024-03-12 16:30:25 +0530

    fix

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h3 className="bg-puprple-500">App</h3>
+    </div>
+  );
+}
+
+export default App;
